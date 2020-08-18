a0c9b3e5a8a92a72a6fcc6e44e4fdb1b1a02b07b
beyondsundae <p00000rice@gmail.com>
2020-08-19 01:34:10 +0700

    Added background

---
+++ src/Components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Section1 from './Pages/Section1'
+import Section2 from './Pages/Section2'
+
+function Home() {
+    return (
+        <div>
+            <Section1/>
+            <Section2/>
+        </div>
+    )
+}
+
+export default Home
---
+++ src/Components/Pages/Section2.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Section2() {
+    return (
+        <div className="section2">
+           
+        </div>
+    )
+}
+
+export default Section2
