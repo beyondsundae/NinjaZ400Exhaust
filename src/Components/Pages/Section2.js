976e1ee841b361811ab9e23e715a8c26beb8d04e
beyondsundae <p00000rice@gmail.com>
2020-08-25 22:52:21 +0700

    SoundClound not comfortable for mobile user. So, I used MP3Player and put those to array and use map method :)

--- src/Components/Pages/Section2.js
+++ src/Components/Pages/Section2.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import ReactPlayer from "react-player"
-
 import AwesomeSlider from 'react-awesome-slider';
 import 'react-awesome-slider/dist/styles.css';
 
+
+import AudioPlayer, { RHAP_UI } from "react-h5-audio-player";
+import 'react-h5-audio-player/lib/styles.css';
+
+import N400y from "../Cover/400y.jpg"
+import RAkraCarbon from "../Cover/Real AkraCarbon.jpg"
+import RAkraTita from "../Cover/Real AkraTita.jpg"
+import FAkraGP from "../Cover/Fake Akra GP.jpg"
+import RLeolv10 from "../Cover/Real Leo lv10.jpg"
+import RSCCRT from "../Cover/Real SC CRT.jpg"
+import FSCShorty from "../Cover/Fake SC Shorty.jpg"
+import FSCCRT from "../Cover/Fake SC CRT.jpg"
+import FSCCNC from "../Cover/Fake SC CNC.jpg"
 
 const Section2 = () => {
 
@@ -16,6 +28,45 @@ const Section2 = () => {
         }
     })
 
+    const Data = [
+        {
+            Pic:N400y,
+            Link: "https://www.mboxdrive.com/400y.mp3",
+            Message: ""}, 
+        {
+            Pic:RAkraCarbon,
+            Link: "https://www.mboxdrive.com/Real_AkraCarbon.mp3",
+            Message: ""}, 
+        {
+            Pic:RAkraTita,
+            Link: "https://www.mboxdrive.com/Real_AkraTita.mp3",
+            Message: ""}, 
+        {
+            Pic:FAkraGP,
+            Link: "https://www.mboxdrive.com/Fake_Akra_GP.mp3",
+            Message: ""}, 
+        {
+            Pic:RLeolv10,
+            Link: "https://www.mboxdrive.com/Real_Leo_lv10.mp3",
+            Message: ""}, 
+        {
+            Pic:RSCCRT,
+            Link: "https://www.mboxdrive.com/Real_SC_CRT.mp3",
+            Message: ""}, 
+        {
+            Pic:FSCShorty,
+            Link: "https://www.mboxdrive.com/Fake_SC_Shorty.mp3",
+            Message: ""}, 
+        {
+            Pic:FSCCRT,
+            Link: "https://www.mboxdrive.com/Fake_SC_CRT.mp3",
+            Message: ""}, 
+        {
+            Pic:FSCCNC,
+            Link: "https://www.mboxdrive.com/Fake_SC_CNC.mp3",
+            Message: ""}
+    ];
+
     return (
         <div className="section2">
            <div className="container-fluid border border-danger" style={Style.Menu}/>
@@ -23,86 +74,33 @@ const Section2 = () => {
            <div className="container-fluid border border-danger" style={Style.Content}>
                <div className="row" style={{height: "91vh", paddingTop: "2vh"}}>
                    <div className="col-6 col-md-12 col-lg-6 offset-lg-6 border border-danger" >
-                       <div data-aos="fade-up" data-aos-anchor-placement="center-center">
-                           {/* xxxx */}
+                       <div  data-aos="fade-up" data-aos-anchor-placement="center-center">
 
-                            {/* <ReactPlayer
-                                    url="https://soundcloud.com/libliss/ninja400"
-                                    width='640px'
-                                    height='640px'
-                                    playing={false}
-                                    muted='true'
-                                /> */}
+                            <AwesomeSlider infinite={false} transitionDelay="0" >        
+                                {Data.map((item, index) =>{
+                                    return(
+                                        <div className="row d-flex justify-content-center border border-danger">
+                                            <div><img src={item.Pic} style={{height:"50vh"}}/></div>
 
-                            <AwesomeSlider>
-                                <div><ReactPlayer
-                                    url="https://soundcloud.com/libliss/ninja400zeus"
-                                    width='640px'
-                                    height='500px'
-                                    playing={false}
-                                    muted='true'
-                                /></div>
-                                <div><ReactPlayer
-                                    url="https://soundcloud.com/libliss/ninja400realakracarbon"
-                                    width='640px'
-                                    height='500px'
-                                    playing={false}
-                                    muted='true'
-                                />xxx</div>
-                                <div><ReactPlayer
-                                    url="https://soundcloud.com/libliss/ninja400realakratitanium"
-                                    width='640px'
-                                    height='500px'
-                                    playing={false}
-                                    muted='true'
-                                />xxx</div>
-                                <div><ReactPlayer
-                                    url="https://soundcloud.com/libliss/ninja400fakegpr1"
-                                    width='640px'
-                                    height='500px'
-                                    playing={false}
-                                    muted='true'
-                                />xxx</div>
-                                <div><ReactPlayer
-                                    url="https://soundcloud.com/libliss/ninja400realleovincelv10"
-                                    width='640px'
-                                    height='500px'
-                                    playing={false}
-                                    muted='true'
-                                />xxx</div>
-                                <div><ReactPlayer
-                                    url="https://soundcloud.com/libliss/ninja400realsccrt"
-                                    width='640px'
-                                    height='500px'
-                                    playing={false}
-                                    muted='true'
-                                />xxx</div>
-                                <div><ReactPlayer
-                                    url="https://soundcloud.com/libliss/sc-project-shorty-titanium-ninja400"
-                                    width='640px'
-                                    height='500px'
-                                    playing={false}
-                                    muted='true'
-                                />xxx</div>
-                                <div><ReactPlayer
-                                    url="https://soundcloud.com/libliss/ninja400fakesc"
-                                    width='640px'
-                                    height='500px'
-                                    playing={false}
-                                    muted='true'
-                                />xxx</div>
-                                <div><ReactPlayer
-                                    url="https://soundcloud.com/libliss/ninja400fakesccnc"
-                                    width='640px'
-                                    height='500px'
-                                    playing={false}
-                                    muted='true'
-                                />xxx</div>
-                                
-                                
-                            </AwesomeSlider>
-                            
+                                            <div className="col-9 border border-danger" style={{position: "relative", bottom: "55px"}}>
+                                                <AudioPlayer style={{ height: '50px',width: "auto", backgroundColor: "transparent" }}
+                                                    src={item.Link}
+                                                    autoPlayAfterSrcChange={false}
+                                                    showJumpControls={false}
+                                                    layout="horizontal-reverse"
+                                                    customProgressBarSection={[RHAP_UI.PROGRESS_BAR]}
+                                                    customControlsSection={[RHAP_UI.MAIN_CONTROLS]}
+                                                />
+                                            </div>
 
+                                            <div className="col-10 border border-danger" style={{bottom:"45px"}}>
+                                                {index}
+                                            </div>
+                                        </div>
+                                        )
+                                    })}
+
+                            </AwesomeSlider>        
                        </div>
                        
                    </div>
